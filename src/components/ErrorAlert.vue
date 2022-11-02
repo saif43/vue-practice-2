60e35460bdb60d0363ed9b68ff9c4cf125fccb49
Saif Ahmed Anik <saif.ahmed.anik.0@gmail.com>
2022-11-02 13:27:31 +0600

    9.12 Slot example

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,19 @@
+<template>
+<dialog open>
+    <slot></slot>
+</dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+dialog {
+    margin: 0;
+    left: 20%;
+    width: 20%;
+}
+</style>>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,12 +1,43 @@
 <template>
     <div>
         <h4>Manage Goals</h4>
-        <input type="text" name="" id="">
+        <input type="text" ref="goalInput">
+        <button @click="setGoal">Set Goal</button>
+
+        <ErrorAlert v-if="hasInputError">
+            <h2>Input is invalid</h2>
+            <p>Please enter few words...</p>
+            <button @click="confirmError">Ok</button>
+        </ErrorAlert>
+        
+        
     </div>
 </template>
 <script>
+import ErrorAlert from './ErrorAlert.vue';
+
 export default {
-    name: 'ManageGoals'
+    name: 'ManageGoals',
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            hasInputError: false
+        }
+    },
+    methods: {
+        setGoal(){
+            const enterValue = this.$refs.goalInput.value;
+
+            if(enterValue === ''){
+                this.hasInputError = true;
+            }
+        },
+        confirmError(){
+            this.hasInputError = false;
+        }
+    }
 }
 </script>
 <style lang="">
